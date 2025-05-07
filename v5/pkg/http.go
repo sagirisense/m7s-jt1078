b74e9702937cdd2464294da6e981e39403ddd65e
CDCDDCDC <792192820@qq.com>
2025-05-07 12:20:23 +0800

    refactor: 回调支持https（跳过证书验证）

--- v5/pkg/http.go
+++ v5/pkg/http.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"crypto/tls"
 	"github.com/go-resty/resty/v2"
 	"time"
 )
@@ -8,6 +9,8 @@ import (
 func onNoticeEvent(url string, httpBody map[string]any) {
 	client := resty.New()
 	client.SetTimeout(1 * time.Second)
+	// 跳过证书验证
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
 	_, _ = client.R().
 		SetBody(httpBody).
 		ForceContentType("application/json; charset=utf-8").
